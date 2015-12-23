f64dceabb2c0277d268029940773aa5d5bcef78b
willians <williamsnieves@gmail.com>
2015-12-23 00:12:00 -0430

    added angular material to store

--- public/js/app.js
+++ public/js/app.js
@@ -3,13 +3,32 @@
  */
 /// <reference path="angular/angular.d.ts" />
 var angular = require("angular");
-angular.module('exampleApp', [])
+angular.module('exampleApp', ['ngMaterial', 'ngMdIcons'])
+    .controller("appCtrl", ['$scope', '$mdSidenav', function ($scope, $mdSidenav) {
+        $scope.openLeftMenu = function () {
+            console.log("abrir");
+            $mdSidenav('left').toggle();
+        };
+    }])
+    .controller('ListCtrl', ['$scope', function ($scope) {
+        $scope.toppings = [
+            { name: 'Pepperoni', wanted: true },
+            { name: 'Sausage', wanted: false },
+            { name: 'Black Olives', wanted: true },
+            { name: 'Green Peppers', wanted: false }
+        ];
+        $scope.messages = [
+            { id: 1, title: "Message A", selected: false },
+            { id: 2, title: "Message B", selected: true },
+            { id: 3, title: "Message C", selected: true },
+        ];
+    }])
     .directive('testDirective', function () {
     return {
         restrict: 'E',
         templateUrl: '../partials/my-template.html'
     };
 });
-var test1 = "hola willow";
+var test1 = "hola willow pu";
 console.log(test1);
 //# sourceMappingURL=app.js.map
